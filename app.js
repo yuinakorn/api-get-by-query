c89ed1de6241f0243d2dd6fea83b9ffdbea20d9d
yuinakorn@hotmail.com <yuinakorn@hotmail.com>
2022-09-02 11:13:11 +0700

    upload for hosxp

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
 require('dotenv').config()
-
+const mysql = require('mysql')
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 80
 
-const mysql = require('mysql')
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({extended: true})) // for parsing application/x-www-form-urlencoded
+
 const connection = mysql.createConnection({
     host: process.env.HIS_HOST,
     user: process.env.HIS_USER,
     password: process.env.HIS_PASSWORD,
     database: process.env.HIS_DATABASE,
     port: process.env.HIS_PORT,
+    charset: process.env.CHARSET
 })
 
+// optional
+connection.connect(function (err) {
+    if (!err) {
+        connection.query("SET NAMES UTF8")
+        console.log('connected as id ' + connection.threadId);
+    } else {
+        console.error('error connecting: ' + err.stack);
+    }
+});
 
-app.get('/', async (req, res) => {
-    let query = req.query.query
+app.get('/', (req, res) => {
+    res.send({"message": "OK"})
+})
+
+app.post('/', async (req, res) => {
+    let query = req.body.script
     await connection.query(query, (err, result) => {
-        // console.log(query)
         try {
             if (err) {
                 res.status(400)
@@ -26,11 +41,10 @@ app.get('/', async (req, res) => {
                     "error": err
                 })
             } else {
+
+                // res.charset = 'tis620'
                 res.status(200)
-                res.send({
-                    "result": result,
-                    "message": "ok"
-                })
+                res.send(result)
             }
         } catch (err) {
             res.send({"message": err})
